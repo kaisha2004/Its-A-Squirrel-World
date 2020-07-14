811a6036f63475bca9dc6cd420a9efd5d29059d9
KaiNJ <smith.kaisha04@gmail.com>
2020-07-14 11:35:08 -0400

    working on JS setup

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 async function storeData() {
   const url = `https://data.cityofnewyork.us/resource/vfnx-vebw.json`
   // Write the rest of the code here.
-    const response = await axios.get(url)
-    console.log(response)
+  const response = await axios.get(url)
+  console.log(response.data)
+  const data = response.data
+ 
+  // function eat(data) {
+  //   const arr = []
+  //   for (let 1 = 0; i < arr.length; i++) {
+  //     if (element) {
+  //       arr.push(i);
+  //     }
+  //   }
+  //   console.log(arr.length)
+  //   return arr.length
+  // }
   
-}
-  storeData()
+} 
+storeData()
+  
+// // capture array
+// // filter out only "true"
+// // grab the array.length
