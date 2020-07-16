a36b739eb01f57d4c834f71d9d71e9a588e9f11b
KaiNJ <smith.kaisha04@gmail.com>
2020-07-16 11:36:40 -0400

    finished MVP

--- app.js
+++ app.js
@@ -3,10 +3,17 @@
 // GAME RESET
 
 const resetBTN = document.querySelector('#reset');
-resetBTN.addEventListener('click', (e) => {
-  e.preventDefault();
-  const squirrels = document.querySelector('.squirrels');
-  squirrels.style.display = 'block';
+resetBTN.addEventListener('click', () => {
+ 
+  const squirrel1 = document.querySelector('#squirrel1');
+  const squirrel2 = document.querySelector('#squirrel2');
+  const squirrel3 = document.querySelector('#squirrel3');
+  const squirrel4 = document.querySelector('#squirrel4');
+  squirrel1.style.display = 'block';
+  squirrel2.style.display = 'block';
+  squirrel3.style.display = 'block';
+  squirrel4.style.display = 'block';
+
 })
 
 // FOR EATING
@@ -58,24 +65,24 @@ squirrel1.addEventListener('click', (e) => {
       const answerB = document.querySelector('#answerB')
       answerB.addEventListener('click', (e) => {
         e.preventDefault();
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
         const status_change = document.querySelector('#status')
-      win_status.style.display = 'block';
-      status_change.innerText = 'You are incorrect :(';
+        win_status.style.display = 'block';
+        status_change.innerText = 'You are incorrect :(';
         facts.style.display = 'block';
-      
-    })
-    const answerC = document.querySelector('#answerC')
+
+      })
+      const answerC = document.querySelector('#answerC')
       answerC.addEventListener('click', (e) => {
-      e.preventDefault()
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
+        e.preventDefault()
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
         const status_change = document.querySelector('#status')
-      win_status.style.display = 'block';
-      status_change.innerText = 'You are incorrect :(';
+        win_status.style.display = 'block';
+        status_change.innerText = 'You are incorrect :(';
         facts.style.display = 'block';
-    })
+      })
 
     }
     catch (error) {
@@ -107,79 +114,79 @@ squirrel2.addEventListener('click', (e) => {
   btnC.innerHTML = '33';
 
 
-async function storeDataKuks() {
-  const url = `https://data.cityofnewyork.us/resource/vfnx-vebw.json`
-  try {
-    const response = await axios.get(url)
-    function kukking() {
-      const kukSquirrels = []
-      const squirrels = response.data
-      squirrels.forEach(squirrel => {
-        const kuk = squirrel.kuks
-        if (kuk) {
-          kukSquirrels.push(squirrel);
+  async function storeDataKuks() {
+    const url = `https://data.cityofnewyork.us/resource/vfnx-vebw.json`
+    try {
+      const response = await axios.get(url)
+      function kukking() {
+        const kukSquirrels = []
+        const squirrels = response.data
+        squirrels.forEach(squirrel => {
+          const kuk = squirrel.kuks
+          if (kuk) {
+            kukSquirrels.push(squirrel);
+          }
+        })
+
+        const filtered = () => {
+          const filter = kukSquirrels.filter((a, b) => kukSquirrels.indexOf(a) === b)
+          return (filter.length)
         }
+        filtered()
+        return filtered()
+
+      }
+      kukking()
+
+      const answerC = document.querySelector('#answerC')
+      answerC.addEventListener('click', (e) => {
+        e.preventDefault();
+        const kukked = kukking();
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
+        if (kukked === 33) {
+          win_status.style.display = 'block';
+          status_change.innerText = 'You are correct!';
+          facts.style.display = 'block';
+          squirrel_facts.innerText = 'The kuk is a sharp bark of alarm and indicates the squirrel is in imminent danger and that a predator is close by.'
+        }
+
       })
 
-      const filtered = () => {
-        const filter = kukSquirrels.filter((a, b) => kukSquirrels.indexOf(a) === b)
-        return (filter.length)
-      }
-      filtered()
-      return filtered()
-
-    }
-    kukking()
-
-    const answerC = document.querySelector('#answerC')
-    answerC.addEventListener('click', (e) => {
-      e.preventDefault();
-      const kukked = kukking();
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      if (kukked === 33) {
+      const answerB = document.querySelector('#answerB')
+      answerB.addEventListener('click', (e) => {
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
         win_status.style.display = 'block';
-        status_change.innerText = 'You are correct!';
+        status_change.innerText = 'You are incorrect :(';
         facts.style.display = 'block';
         squirrel_facts.innerText = 'The kuk is a sharp bark of alarm and indicates the squirrel is in imminent danger and that a predator is close by.'
-      }
+      })
 
-    })
+      const answerA = document.querySelector('#answerA')
+      answerA.addEventListener('click', (e) => {
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
+        win_status.style.display = 'block';
+        status_change.innerText = 'You are incorrect :(';
+        facts.style.display = 'block';
+        squirrel_facts.innerText = 'The kuk is a sharp bark of alarm and indicates the squirrel is in imminent danger and that a predator is close by.'
+      })
 
-    const answerB = document.querySelector('#answerB')
-    answerB.addEventListener('click', (e) => {
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      win_status.style.display = 'block';
-      status_change.innerText = 'You are incorrect :(';
-      facts.style.display = 'block';
-      squirrel_facts.innerText = 'The kuk is a sharp bark of alarm and indicates the squirrel is in imminent danger and that a predator is close by.'
-    })
-    
-    const answerA = document.querySelector('#answerA')
-    answerA.addEventListener('click', (e) => {
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      win_status.style.display = 'block';
-      status_change.innerText = 'You are incorrect :(';
-      facts.style.display = 'block';
-      squirrel_facts.innerText = 'The kuk is a sharp bark of alarm and indicates the squirrel is in imminent danger and that a predator is close by.'
-    })
+    }
+    catch (error) {
+      console.log(`Error ${error}`)
+    }
+
 
   }
-  catch (error) {
-    console.log(`Error ${error}`)
-  }
-  
-  
-}
-storeDataKuks()
+  storeDataKuks()
 
 
 })
@@ -204,83 +211,83 @@ squirrel3.addEventListener('click', (e) => {
   btnC.innerHTML = '61';
 
 
-async function storeDataApproach() {
-  const url = `https://data.cityofnewyork.us/resource/vfnx-vebw.json`
-  try {
-    const response = await axios.get(url)
-    function approaching() {
-      const appSquirrels = []
-      const squirrels = response.data
-      squirrels.forEach(squirrel => {
-        const app = squirrel.approaches
-        if (app) {
-          appSquirrels.push(squirrel);
+  async function storeDataApproach() {
+    const url = `https://data.cityofnewyork.us/resource/vfnx-vebw.json`
+    try {
+      const response = await axios.get(url)
+      function approaching() {
+        const appSquirrels = []
+        const squirrels = response.data
+        squirrels.forEach(squirrel => {
+          const app = squirrel.approaches
+          if (app) {
+            appSquirrels.push(squirrel);
+          }
+        })
+
+        const filtered = () => {
+          const filter = appSquirrels.filter((a, b) => appSquirrels.indexOf(a) === b)
+          return (filter.length)
         }
+        filtered()
+        return filtered()
+
+      }
+      approaching()
+
+      const answerC = document.querySelector('#answerC')
+      answerC.addEventListener('click', (e) => {
+        e.preventDefault();
+        const approached = approaching();
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
+        if (approached === 61) {
+          win_status.style.display = 'block';
+          status_change.innerText = 'You are correct!';
+          facts.style.display = 'block';
+          squirrel_facts.innerText = 'Squirrels front teeth never stop growing and can grow up to 6 inches a year.'
+        }
+
       })
 
-      const filtered = () => {
-        const filter = appSquirrels.filter((a, b) => appSquirrels.indexOf(a) === b)
-        return (filter.length)
-      }
-      filtered()
-      return filtered()
-
-    }
-    approaching()
-
-    const answerC = document.querySelector('#answerC')
-    answerC.addEventListener('click', (e) => {
-      e.preventDefault();
-      const approached = approaching();
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      if (approached === 61) {
+      const answerB = document.querySelector('#answerB')
+      answerB.addEventListener('click', (e) => {
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
         win_status.style.display = 'block';
-        status_change.innerText = 'You are correct!';
+        status_change.innerText = 'You are incorrect :(';
         facts.style.display = 'block';
         squirrel_facts.innerText = 'Squirrels front teeth never stop growing and can grow up to 6 inches a year.'
-      }
+      })
 
-    })
+      const answerA = document.querySelector('#answerA')
+      answerA.addEventListener('click', (e) => {
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
+        win_status.style.display = 'block';
+        status_change.innerText = 'You are incorrect :(';
+        facts.style.display = 'block';
+        squirrel_facts.innerText = 'Squirrels front teeth never stop growing and can grow up to 6 inches a year.'
+      })
 
-    const answerB = document.querySelector('#answerB')
-    answerB.addEventListener('click', (e) => {
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      win_status.style.display = 'block';
-      status_change.innerText = 'You are incorrect :(';
-      facts.style.display = 'block';
-      squirrel_facts.innerText = 'Squirrels front teeth never stop growing and can grow up to 6 inches a year.'
-    })
-    
-    const answerA = document.querySelector('#answerA')
-    answerA.addEventListener('click', (e) => {
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      win_status.style.display = 'block';
-      status_change.innerText = 'You are incorrect :(';
-      facts.style.display = 'block';
-      squirrel_facts.innerText = 'Squirrels front teeth never stop growing and can grow up to 6 inches a year.'
-    })
+    }
+    catch (error) {
+      console.log(`Error ${error}`)
+    }
+
 
   }
-  catch (error) {
-    console.log(`Error ${error}`)
-  }
-  
-  
-}
-storeDataApproach()
+  storeDataApproach()
 
 
 })
- 
+
 
 
 // FOR FORAGING 
@@ -301,79 +308,79 @@ squirrel4.addEventListener('click', (e) => {
   btnC.innerHTML = '371';
 
 
-async function storeDataForaging() {
-  const url = `https://data.cityofnewyork.us/resource/vfnx-vebw.json`
-  try {
-    const response = await axios.get(url)
-    function foraging() {
-      const forSquirrels = []
-      const squirrels = response.data
-      squirrels.forEach(squirrel => {
-        const forage = squirrel.foraging
-        if (forage) {
-          forSquirrels.push(squirrel);
+  async function storeDataForaging() {
+    const url = `https://data.cityofnewyork.us/resource/vfnx-vebw.json`
+    try {
+      const response = await axios.get(url)
+      function foraging() {
+        const forSquirrels = []
+        const squirrels = response.data
+        squirrels.forEach(squirrel => {
+          const forage = squirrel.foraging
+          if (forage) {
+            forSquirrels.push(squirrel);
+          }
+        })
+
+        const filtered = () => {
+          const filter = forSquirrels.filter((a, b) => forSquirrels.indexOf(a) === b)
+          return (filter.length)
         }
+        filtered()
+        return filtered()
+
+      }
+      foraging()
+
+      const answerA = document.querySelector('#answerA')
+      answerA.addEventListener('click', (e) => {
+        e.preventDefault();
+        const foraged = foraging();
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
+        if (foraged === 474) {
+          win_status.style.display = 'block';
+          status_change.innerText = 'You are correct!';
+          facts.style.display = 'block';
+          squirrel_facts.innerText = 'Squirrels can find food buried underneath a foot of snow and may increase their body weight by 25% during winter.'
+        }
+
       })
 
-      const filtered = () => {
-        const filter = forSquirrels.filter((a, b) => forSquirrels.indexOf(a) === b)
-        return (filter.length)
-      }
-      filtered()
-      return filtered()
-
-    }
-    foraging()
-
-    const answerA = document.querySelector('#answerA')
-    answerA.addEventListener('click', (e) => {
-      e.preventDefault();
-      const foraged = foraging();
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      if (foraged === 474) {
+      const answerB = document.querySelector('#answerB')
+      answerB.addEventListener('click', (e) => {
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
         win_status.style.display = 'block';
-        status_change.innerText = 'You are correct!';
+        status_change.innerText = 'You are incorrect :(';
         facts.style.display = 'block';
         squirrel_facts.innerText = 'Squirrels can find food buried underneath a foot of snow and may increase their body weight by 25% during winter.'
-      }
+      })
 
-    })
+      const answerC = document.querySelector('#answerC')
+      answerC.addEventListener('click', (e) => {
+        const win_status = document.querySelector('.win_status');
+        const facts = document.querySelector('.facts')
+        const status_change = document.querySelector('#status')
+        const squirrel_facts = document.querySelector('#squirrel_facts');
+        win_status.style.display = 'block';
+        status_change.innerText = 'You are incorrect :(';
+        facts.style.display = 'block';
+        squirrel_facts.innerText = 'Squirrels can find food buried underneath a foot of snow and may increase their body weight by 25% during winter.'
+      })
 
-    const answerB = document.querySelector('#answerB')
-    answerB.addEventListener('click', (e) => {
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      win_status.style.display = 'block';
-      status_change.innerText = 'You are incorrect :(';
-      facts.style.display = 'block';
-      squirrel_facts.innerText = 'Squirrels can find food buried underneath a foot of snow and may increase their body weight by 25% during winter.'
-    })
-    
-    const answerC = document.querySelector('#answerC')
-    answerC.addEventListener('click', (e) => {
-      const win_status = document.querySelector('.win_status');
-      const facts = document.querySelector('.facts')
-      const status_change = document.querySelector('#status')
-      const squirrel_facts = document.querySelector('#squirrel_facts');
-      win_status.style.display = 'block';
-      status_change.innerText = 'You are incorrect :(';
-      facts.style.display = 'block';
-      squirrel_facts.innerText = 'Squirrels can find food buried underneath a foot of snow and may increase their body weight by 25% during winter.'
-    })
+    }
+    catch (error) {
+      console.log(`Error ${error}`)
+    }
+
 
   }
-  catch (error) {
-    console.log(`Error ${error}`)
-  }
-  
-  
-}
-storeDataForaging()
+  storeDataForaging()
 
 })
 
